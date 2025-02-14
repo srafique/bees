88d1b208b5345cd78891ef01699ac46f50da0a24
Salman Rafique <rafiqsalman@gmail.com>
2025-02-13 22:18:14 -0800

    changes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // Enables `next export`
+  images: { unoptimized: true }
 };
 
 export default nextConfig;
