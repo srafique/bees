9742d2256664abfc98fb3ae1ef85e6498e05afe2
Salman Rafique <rafiqsalman@gmail.com>
2025-02-26 18:41:33 -0800

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,8 @@ import { PopupWidget }  from "@/components/PopupWidget";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Bee Craft Handyman Service",
-  description: "Safe and humane bee & wasp removal services.",
+  title: "Bee Craft Handyman Service | Reliable Home Repairs & Renovations",
+  description: "Bee Craft Handyman Service specializes in safe and humane bee & wasp removal, along with expert home repairs and maintenance. Serving Inland Empire, Orange County, and Los Angeles County, we ensure quality, reliability, and eco-friendly solutions.",
 };
 
 export default function RootLayout({
